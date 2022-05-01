4b7277634ac2c2fddaf5b538c0197b4b97bc7149
natdanai.j@aware.co.th <natdanai.j@aware.co.th>
2022-05-02 01:19:43 +0700

    update fix navbar-002

--- resources/js/vue/app.vue
+++ resources/js/vue/app.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="container-fluid px-0">
+  <div class="container-fluid">
     <div class="row">
-      <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2 bg-white shadow">
+      <div class="col-12 col-sm-12 col-md-12 col-lg-2 col-xl-2 bg-white shadow mx-0">
         <Menu></Menu>
       </div>
       <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 content pt-5">
@@ -9,11 +9,6 @@
       </div>
     </div>
   </div>
-  <!-- <div v-else class="container h-100 d-flex justify-content-center">
-       <div class="col-12 col-sm-12 col-md-12 col-lg-10 col-xl-10 content pt-5">
-            <router-view />
-        </div>
-    </div> -->
 </template>
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Prompt&display=swap");
